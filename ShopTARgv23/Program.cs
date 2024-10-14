72d173339966876de44da53facbc15fc41e98e3c
MartinKemppi <dusperin1234@gmail.com>
2024-10-14 12:11:49 +0300

    change branch to kindergarten

---
+++ ShopTARgv23/Program.cs
@@ -0,0 +1,50 @@
+using ShopTARgv23.Data;
+using Microsoft.EntityFrameworkCore;
+using ShopTARgv23.Core.ServiceInterface;
+using ShopTARgv23.ApplicationServices.Services;
+using Microsoft.Extensions.FileProviders;
+
+
+namespace ShopTARgv23
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<IKindergartenServices, KindergartensServices>();
+            
+
+            builder.Services.AddDbContext<ShopTARgv23Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
